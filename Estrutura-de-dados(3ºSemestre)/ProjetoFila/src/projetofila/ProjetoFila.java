42c542d2c74f19a234b6dad84c323f50e1cb75d5
André_Lucas <58487111+andrelucassvt@users.noreply.github.com>
2020-03-06 08:28:18 -0300

    Update ProjetoFila.java

--- Estrutura-de-dados(3ºSemestre)/ProjetoFila/src/projetofila/ProjetoFila.java
+++ Estrutura-de-dados(3ºSemestre)/ProjetoFila/src/projetofila/ProjetoFila.java
@@ -1,1 +1,44 @@
 
+
+package projetofila;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+public class Projetofila {
+
+    public static void main(String[] args) {
+     
+    Queue<String> filaEspera = new LinkedList<>();
+    
+    //ADICIONAR ELEMENTOS NA FILA
+        filaEspera.add("Rodrigo");
+        filaEspera.add("Joao");
+        filaEspera.add("Adriana");
+        
+    //consultar toda a fila
+        System.out.println("Elementos da fila: " + filaEspera);
+        
+    //verificar se a fila esta vazia
+        System.out.println("A fila esta vazia " + filaEspera.isEmpty());    
+    
+    //verificar o tamanho da fila
+        System.out.println("Tamanho da fila: "+filaEspera.size());
+        
+        
+    //verificar se há um elemento especifico na fila
+        String nome = "Ricardo";
+        if(filaEspera.contains(nome))System.out.println(nome + " esta na fila!");
+        else System.out.println(nome + " não esta na fila");
+        
+     //Obtem um elemento da fila (quem esta no inicio da fila)
+        System.out.println("Nome: " + filaEspera.peek());
+        
+     ////remove quem esta no inicio da fila
+        System.out.println("Removendo da fila: " + filaEspera.poll());
+        System.out.println("Nova configuracao: "+ filaEspera);
+        
+        
+    }  
+}
