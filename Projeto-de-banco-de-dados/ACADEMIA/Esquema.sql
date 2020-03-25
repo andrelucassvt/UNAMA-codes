aedaea4e30ac89dd8cc67368ab4012fc2fca5466
andrelucassvt <andrelucassvt99@gmail.com>
2020-03-25 09:06:29 -0300

    Alteração no insert de Aluno e Matricula

--- Projeto-de-banco-de-dados/ACADEMIA/Esquema.sql
+++ Projeto-de-banco-de-dados/ACADEMIA/Esquema.sql
@@ -45,15 +45,15 @@ alter table aluno add constraint pk_aluno unique(cnpj,cpf);
 insert into modalidade values(default,'20',' Natação','60.0'), (default,'20','Futebol','100'),(default,'20','Corrida','30.0'),
 (default,'20','Vôlei','80.0'),(default,'20','Luta','100.0');
 
-insert into aluno values('04041884','123456','André Lucas Barbosa Salvador'),('1019823','123456','Igor da Silva Soares'),
-('8979175','123456','Andrei Victor Leao'),('01862818','123456','Diogo Felipe'),('10101927','123456','Isabela Lima'),
-('10282734','123456','Kevin Sarges'),('22356312','123456','Erick Lima'),('19198265','123456','Daniel Henriques'),
-('10109275','123456','Wagner Barbalho'),('1277853','123456','Caio Rafael');
+insert into aluno values('04041884','1443456','André Lucas Barbosa Salvador'),('1019823','15156456','Igor da Silva Soares'),
+('8979175','12312441','Andrei Victor Leao'),('01862818','1142556','Diogo Felipe'),('10101927','91973556','Isabela Lima'),
+('10282734','1949376','Kevin Sarges'),('22356312','129398656','Erick Lima'),('19198265','1148636','Daniel Henriques'),
+('10109275','1937455','Wagner Barbalho'),('1277853','19394556','Caio Rafael');
 
-insert into matricula values (default,'04041884','4','A','2020-03-19'),(default,'1019823','2','A','2020-03-19'),
-(default,'8979175','1','A','2020-03-19'),(default,'01862818','3','A','2020-03-19'),(default,'10101927','5','A','2020-03-19'),
-(default,'10282734','4','A','2020-03-19'),(default,'22356312','2','A','2020-03-19'),(default,'19198265','5','A','2020-03-19'),
-(default,'10109275','1','A','2020-03-19'),(default,'1277853','3','A','2020-03-19');
+insert into matricula values (default,'04041884','123456','4','A','2020-03-19'),(default,'1019823','123456','2','A','2020-03-19'),
+(default,'8979175','123456','1','A','2020-03-19'),(default,'01862818','123456','3','A','2020-03-19'),(default,'10101927','123456','5','A','2020-03-19'),
+(default,'10282734','123456','4','A','2020-03-19'),(default,'22356312','123456','2','A','2020-03-19'),(default,'19198265','123456','5','A','2020-03-19'),
+(default,'10109275','442145','1','A','2020-03-19'),(default,'1277853','442145','3','A','2020-03-19');
 
 
 
